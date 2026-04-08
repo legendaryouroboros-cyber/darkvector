8aed58c656f90cbcc1a3f9e3ace36dadc8f5e1c0
Бекжан <legendaryouroboros@gmail.com>
2026-04-09 03:39:38 +0600

    ss

--- index.php
+++ index.php
@@ -6,31 +6,33 @@ $botUrl = "https://api.telegram.org/bot{$token}";
 $input = file_get_contents('php://input');
 $update = json_decode($input, true);
 
-if (!isset($update['message'])) exit;
+if (!isset($update['message'])) {
+    // Тест - открой в браузере
+    $ch = curl_init("{$botUrl}/getMe");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $result = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+    echo "RESULT: " . $result . "<br>ERROR: " . $error;
+    exit;
+}
 
 $chatId = $update['message']['chat']['id'];
 $text = $update['message']['text'] ?? '';
 
 if ($text === '/start') {
     $reply = 'Привет! Я работаю 🤖';
-} elseif ($text === '/help') {
-    $reply = 'Доступные команды: /start, /help';
 } else {
     $reply = 'Ты написал: ' . $text;
 }
 
-$data = json_encode([
-    'chat_id' => $chatId,
-    'text' => $reply
-]);
-
+$data = json_encode(['chat_id' => $chatId, 'text' => $reply]);
 $ch = curl_init("{$botUrl}/sendMessage");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data)
-]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
